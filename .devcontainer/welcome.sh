1c10d9cf83da73e76db25dce94ea226b0fec72ca
Seamus <seamusrobertmurphy@gmail.com>
2025-11-11 23:07:32 -0800

    rstudio auto

---
+++ .devcontainer/welcome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "RStudio Server is running. Click the 'Ports' tab below and open port 8787 in a browser to access RStudio."
+# The RStudio server might need an explicit start command depending on the base image, 
+# but the rocker images usually handle this automatically. The ports tab should become active.
+
